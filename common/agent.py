3f784ef84ed89f8b3024d2f2cadef252640b574e
jp013718 <jp013718@ohio.edu>
2025-08-05 15:06:30 -0400

    Modularization

---
+++ common/agent.py
@@ -0,0 +1,42 @@
+import numpy as np
+import gymnasium.spaces as spaces
+
+class Agent:
+  def __init__(
+      self,
+      name: str,
+      action_space: spaces.Space,
+      x: np.float32=0.0, 
+      y: np.float32=0.0,
+      model_type: str|None=None
+  ):
+    
+    self.name = name
+    self.action_space = action_space
+    self.x = x
+    self.y = y
+    self.speed = 0
+    self.heading = 0
+    self.angular_vel = 0
+    self.model_type = model_type
+    
+    if self.model_type:
+      self._load_model(self.model_type)
+    else:
+      self.model = None
+
+  def get_action(self, observation):
+    if self.model:
+      self.model.get_action(observation)
+    else:
+      return self.action_space.sample()
+
+  def reset(self):
+    self.x = 0
+    self.y = 0
+    self.speed = 0
+    self.heading = 0
+    self.angular_vel = 0
+
+  def _load_model(self, model):
+    raise NotImplementedError("Model selection not implemented")
--- environment/envs/marl_env.py
+++ environment/envs/marl_env.py
@@ -4,6 +4,9 @@ import gymnasium.spaces as spaces
 
 from pettingzoo import ParallelEnv
 from copy import copy
+from numpy.random import Generator
+
+from common.agent import Agent
 
 class MarlEnvironment(ParallelEnv):
   metadata = {
@@ -20,8 +23,11 @@ class MarlEnvironment(ParallelEnv):
       max_speed: int=5, 
       max_accel: np.float32=1.0, 
       max_angular_accel: np.float32=0.2*np.pi, 
-      render_fps: int|None=None, render_state: bool=True
-    ):
+      render_fps: int|None=None, 
+       render_mode: bool=True,
+      render_vectors: bool=True,
+  ):
+    
     self.mapsize = mapsize
     self.max_timesteps = max_timesteps
     self.num_agents = num_agents
@@ -32,20 +38,22 @@ class MarlEnvironment(ParallelEnv):
     try:
       assert num_near_agents < self.num_agents
     except AssertionError as e:
-      raise e(f"num_near_agents must be less than num_agents. You chose num_agents as {self.num_agents} and num_near_agents as {num_near_agents}")
+      raise e(f"num_near_agents must be less than num_agents. You chose num_agents as {num_agents} and num_near_agents as {num_near_agents}")
     
     self.num_near_agents = num_near_agents
 
-    self.render_state = render_state
+    self.render_mode =  render_mode
     self.render_fps = render_fps if render_fps else self.metadata["render_fps"]
+    self.render_vectors = render_vectors
 
     self.targets_x = [0]*self.num_agents
     self.targets_y = [0]*self.num_agents
 
-    self.agents_x = [0]*self.num_agents
-    self.agents_y = [0]*self.num_agents
-    self.agents_speed = [0]*self.num_agents
-    self.agents_heading = [0]*self.num_agents
+    # self.agents_x = [0]*self.num_agents
+    # self.agents_y = [0]*self.num_agents
+    # self.agents_speed = [0]*self.num_agents
+    # self.agents_heading = [0]*self.num_agents
+    # self.agents_angular_vel = [0]*self.num_agents
 
     self.timestep = 0
     self.possible_agents = ["agent"]
@@ -72,22 +80,59 @@ class MarlEnvironment(ParallelEnv):
     }
 
     self.action_spaces = {
-      "agent": spaces.Box(low=(0, 0), high=(self.max_angular_accel, self.max_accel), shape=(2,), dtype=np.float32)
+      "agent": spaces.Box(low=(-self.max_angular_accel, -self.max_accel), high=(self.max_angular_accel, self.max_accel), shape=(2,), dtype=np.float32)
     }
 
-  def reset(self, seed=None, options=None):
-    pass
+    self.agents = [Agent(f"agent_{i}", self.action_space("agent")) for i in range(num_agents)]
 
-  def step(self, actions):
-    pass
+  def reset(self, seed=None, options=None):
+    super().reset(seed=seed)
+
+    self.timestep = 0
+
+    for i, agent in enumerate(self.agents):
+      agent.reset()
+      agent.x, agent.y = Generator.random(size=2)*self.mapsize
+      self.targets_x[i], self.targets_y[i] = Generator.random(size=2)*self.mapsize
+
+    observations = self._get_obs()
+    infos = self._get_infos()
+
+    return observations, infos
+
+  def step(self):
+    for agent in self.agents:
+      agent.get_action()
+      self.agents_angular_vel[i] += action[0]
+      self.agents_speed[i] += action[1]
+
+      self.agents_heading[i] += self.agents_angular_vel[i]
+      self.agents_x[i] += self.agents_speed[i]*np.cos(self.agents_heading[i])
+      self.agents_y[i] += self.agents_speed[i]*np.sin(self.agents_heading[i])
+
+    observations = self._get_obs()
+    rewards = self._get_rewards(observations, actions)
+    terminations = self._get_terms()
+    truncations = self._get_truncs()
+    infos = self._get_infos()
+
+    self.timestep += 1
+
+    return observations, rewards, terminations, truncations, infos
 
   def _get_obs(self):
     pass
 
-  def _get_infos(self):
+  def _get_rewards(self, observations, actions):
     pass
 
-  def _get_rewards(self, observations, actions):
+  def _get_terms(self):
+    pass
+
+  def _get_truncs(self):
+    return [False]*self.num_agents if self.timestep < self.max_timesteps else [True]*self.num_agents
+  
+  def _get_infos(self):
     pass
 
   def render(self):

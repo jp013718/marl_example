8ce155f7101ef12168e2454cbe7c2387a4590f18
jp013718 <jp013718@ohio.edu>
2025-10-09 11:21:55 -0400

    Fixed experience buffer bug where adding a new memory overwrote all memories already in the buffer. This caused learning to always be performed repeatedly on the most recent memory.

--- models/maddpg/buffer.py
+++ models/maddpg/buffer.py
@@ -9,29 +9,27 @@ class ReplayBuffer:
         self.batch_size = batch_size
         self.n_actions = n_actions
 
-        self.memory = np.array([{}]*max_size)
+        self.memory = [{}]*max_size
 
     def store_transition(self, raw_obs, state, action, reward, raw_obs_, state_, done):
         mem_idx = self.mem_cntr % self.mem_size
-        self.memory[mem_idx].update(
-            {
-                "observation": raw_obs,
-                "state": state,
-                "action": action,
-                "reward": reward,
-                "observation_": raw_obs_,
-                "state_": state_,
-                "done": done,
-            }
-        )
-            
+        self.memory[mem_idx] = {
+            "observation": raw_obs,
+            "state": state,
+            "action": action,
+            "reward": reward,
+            "observation_": raw_obs_,
+            "state_": state_,
+            "done": done,
+        }
+        
         self.mem_cntr += 1
 
     def sample_buffer(self):
         max_mem = min(self.mem_cntr, self.mem_size)
         batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        
-        return self.memory[batch]
+
+        return np.array(self.memory)[batch]
 
 
     def ready(self):
